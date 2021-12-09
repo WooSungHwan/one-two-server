fcd56bb0aa44bda3bf37fa1de79cd18f00d83da9
흑구 <doqndnffo@gmail.com>
2021-12-10 07:39:46 +0900

    cors 매핑 추가.

---
+++ src/main/java/com/blackdog/onetwo/configuration/MvcConfig.java
@@ -0,0 +1,17 @@
+package com.blackdog.onetwo.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .maxAge(3600); // 클라이언트 캐시 1시간.
+    }
+}
