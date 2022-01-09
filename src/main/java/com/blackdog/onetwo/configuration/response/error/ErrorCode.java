322d3b5530f252d3072d150b161235df564ddb51
흑구 <doqndnffo@gmail.com>
2022-01-09 22:27:29 +0900

    error code 추가.

--- src/main/java/com/blackdog/onetwo/configuration/response/error/ErrorCode.java
+++ src/main/java/com/blackdog/onetwo/configuration/response/error/ErrorCode.java
@@ -11,6 +11,7 @@ public enum ErrorCode {
     USER_NOT_FOUND("USER_NOT_FOUND", "유저 정보가 없습니다."),
     KAKAO_USER_NOT_FOUND("KAKAO_USER_NOT_FOUND", "카카오 유저 정보가 없습니다."),
     LOGIN_EXPIRED("LOGIN_EXPIRED", "로그인이 만료되었습니다."),
+    RESOURCE_NOT_FOUND("ENTITY_NOT_FOUND", "요청에 해당하는 리소스 정보가 없습니다."),
     RESOURCE_FORBIDDEN("RESOURCE_FORBIDDEN", "해당 리소스에 접근하실 수 없습니다.");
 
     private String code;
